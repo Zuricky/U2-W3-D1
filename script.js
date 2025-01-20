e8dc529540f8664f172ce542c0a6e3ce527207d6
Luca <zuin.luca1111@gmail.comgit config --global user.email zuin.luca1111@gmail.com>
2025-01-20 17:56:35 +0100

    commit 2

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// ESERCIZIO 1
 class User {
   constructor(firstName, lastName, age, location) {
     this.firstName = firstName;
@@ -5,4 +6,47 @@ class User {
     this.age = age;
     this.location = location;
   }
+
+  compareAge(otherUser) {
+    if (this.age > otherUser.age) {
+      return `${this.firstName} è più vecchio di ${otherUser.firstName}`;
+    } else if (this.age < otherUser.age) {
+      return `${this.firstName} è più giovane di ${otherUser.firstName}`;
+    } else {
+      return `${this.firstName} e ${otherUser.firstName} hanno la stessa età`;
+    }
+  }
 }
+
+const userX = new User("Willy", "Branchi", 19, "Ravenna");
+const userY = new User("Luca", "Terzo", 20, "Vicenza");
+
+console.log(userX.compareAge(userY));
+
+// ESERCIZIO 2
+const pets = [];
+
+class Pet {
+  constructor(petName, owner, species, breed) {
+    this.petName = petName;
+    this.owner = owner;
+    this.species = species;
+    this.breed = breed;
+  }
+}
+
+const formShop = document.getElementById("formShop");
+
+formShop.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const nameInput = document.getElementById("name");
+  const ownerInput = document.getElementById("ownerName");
+  const speciesInput = document.getElementById("species");
+  const breedInput = document.getElementById("breed");
+  const newPet = new Pet(nameInput.value, ownerInput.value, speciesInput.value, breedInput.value);
+
+  pets.push(newPet);
+
+  console.log(newPet);
+  console.log(pets);
+});
